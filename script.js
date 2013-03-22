e1e3b81e3474d296de781039f992f4e448d2bed6
Baldur Þór Emilsson <baldur@baldur.biz>
2013-03-22 18:22:20 +0000

    Added constituencies

--- script.js
+++ script.js
@@ -7,13 +7,24 @@ var party_li = function(letter, info) {
   return crel('li', name, ' (' + letter + ')');
 };
 
+var constituency_li = function(name, info) {
+  return crel('li', crel('a', {'href': info.url}, name));
+};
+
 $(function() {
   var self = this;
   var parties = $('#parties');
+  var constituencies = $('#constituencies');
 
   $.getJSON('parties.json', function(data) {
     for(var partyLetter in data) {
       parties.append(party_li(partyLetter, data[partyLetter]));
     }
   });
+
+  $.getJSON('constituencies.json', function(data) {
+    for(var name in data) {
+      constituencies.append(constituency_li(name, data[name]));
+    }
+  })
 });
