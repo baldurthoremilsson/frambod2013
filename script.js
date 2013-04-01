dbc35e91882c46c4cdf72602fe6d4398719e3cfb
Baldur Þór Emilsson <baldur@baldur.biz>
2013-04-01 22:05:31 +0000

    First step in Backbone'ifying the page
    No longer compiling the json files for all the parties in to one big
    file.

--- script.js
+++ script.js
@@ -1,93 +1,91 @@
+// utils
 var link = function(value, url) {
   if(url)
     return crel('a', {href: url}, value);
   return value;
 };
 
-var party_li = function(info) {
-  var letter = ' (' + info.letter + ')';
-  return crel('li', link(info.name, info.url), letter);
-};
 
-var constituency_li = function(name, info) {
-  return crel('li', crel('a', {href: info.url}, name));
-};
+// models
 
-var candidate_list = function(partyName, info) {
-  var fragment = document.createDocumentFragment();
-  fragment.appendChild(crel('h3', link(partyName, info.url)));
+var Party = Backbone.Model.extend({
+  urlRoot: 'data/',
+});
 
-  info.constituencies.sort(function(a,b) {
-    if(a.name < b.name)
-      return -1;
-    if(a.name > b.name)
-      return 1;
-    return 0;
-  });
-  for(var i in info.constituencies) {
-    var constituency = info.constituencies[i];
-    var ol = crel('ol');
-    fragment.appendChild(crel('h4', link(constituency.name, constituency.url)));
-    for(var j in constituency.list) {
-      var person = constituency.list[j];
-      ol.appendChild(crel('li', link(person.name, person.url)));
+
+// collections
+
+var PartyList = Backbone.Collection.extend({
+  model: Party,
+  url: 'data/partylist.json',
+  comparator: 'letter',
+  populate: function() {
+    var options = {
+      success: _.bind(this.populateHandler, this),
+    };
+    this.sync('read', this, options);
+  },
+  populateHandler: function(partylist) {
+    for(var i = 0; i < partylist.parties.length; i++) {
+      var party = new Party({
+        id: partylist.parties[i],
+      });
+      this.add(party);
+      party.fetch();
     }
-    fragment.appendChild(ol);
-  }
-
-  return fragment;
-};
+  },
+});
 
 
-var parties_list = function(el, data) {
-  var letters = [];
-  var parties = {};
-  for(var i in data.parties) {
-    var p = data.parties[i];
-    letters.push(p.letter);
-    parties[p.letter] = p;
-  }
+// views
 
-  letters.sort();
-  for(var i in letters) {
-    var letter = letters[i];
-    var info = parties[letter];
-    el.append(party_li(info));
-  }
-};
+var PartyListView = Backbone.View.extend({
+  initialize: function() {
+    this.listenTo(this.collection, 'change', this.render);
+  },
+  render: function() {
+    this.$el.empty();
+    var parties = this.collection.slice();
+    for(var i = 0; i < parties.length; i++) {
+      var party = new PartyView({model: parties[i]});
+      this.$el.append(party.render().el);
+    }
+  },
+});
 
-var candidates_list = function(el, data) {
-  for(var i in data.parties) {
-    var party = data.parties[i];
-    var candidates = party.candidates;
-    if(typeof candidates === "undefined")
-      continue;
-    //console.log(party.name);
-    el.append(candidate_list(party.name, candidates));
-  }
-};
+var ConstituenciesView = Backbone.View.extend({
+});
 
+var PartyView = Backbone.View.extend({
+  tagName: 'li',
+  render: function() {
+    var attrs = this.model.attributes;
+    this.$el.append(link(attrs.name, attrs.url));
+    this.$el.append(' (' + attrs.letter + ')');
+    return this;
+  },
+});
+
+var AppView = Backbone.View.extend({
+  initialize: function() {
+    this.partyCollection = new PartyList;
+    this.partylist = this.$('#partylist');
+    this.constituencies = this.$('#constituencies');
+    this.parties = this.$('#parties');
+
+    new PartyListView({
+      collection: this.partyCollection,
+      el: this.partylist,
+    });
+
+    this.partyCollection.populate();
+  },
+});
+
+
+// kickstart the page!
 $(function() {
-  var self = this;
-  var parties = $('#parties');
-  var constituencies = $('#constituencies');
-  var candidates = $('#candidates');
-
-  $.getJSON('data/parties.json', function(data) {
-    parties_list(parties, data);
-    candidates_list(candidates, data);
-  });
-
-  $.getJSON('data/constituencies.json', function(data) {
-    for(var name in data) {
-      constituencies.append(constituency_li(name, data[name]));
-    }
-  });
-
-  return;
-  $.get('data/candidates.json', function(data) {
-    for(var partyName in data) {
-      candidates.append(candidate_list(partyName, data[partyName]));
-    }
+  var app = new AppView({
+    el: $('#frambod2013')
   });
 });
